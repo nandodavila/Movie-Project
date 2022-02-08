e425c8cfb83c57d30e2199c56ed64ddb9310e3a5
Nando <nandodavila94@gmail.com>
2022-02-07 21:16:55 -0500

    user data is seeding

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,13 +1,17 @@
 const db = require('../config/connection');
 const { List, User } = require('../models');
 
-const awardListData = require('./awardListData.json');
 const movieListData = require('./movieListData.json');
+const userData = require('./userData.json');
 
 db.once('open', async () => {
   await List.deleteMany({});
 
   const Lists = await List.insertMany(movieListData);
+
+  await User.deleteMany({});
+
+  const Users = await User.insertMany(userData);
 
   console.log('Movie Lists seeded');
   process.exit(0);
