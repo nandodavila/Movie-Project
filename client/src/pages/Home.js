d4432bfaf3c4193bdd20b66cd2c5b01e60ee27ee
Travis Crocker <Travis.Crocker@outlook.com>
2022-02-09 17:00:00 -0500

    header styleing

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,7 +2,7 @@
 import { useQuery, useMutation} from '@apollo/client';
 // import { QUERY_MATCHUPS } from '../utils/queries';
 import { CREATE_LIST } from '../utils/mutations';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 let apiKey = '8ffb7060';
 let listOfMovie = [];
 let loggedIn = true;
@@ -35,7 +35,21 @@ const Home = () => {
 
   const addMovie = async (event) => {
     console.log('i clicked' + event)
-    console.log(event)
+    let id = event.target.id;
+    fetch(`http://www.omdbapi.com/?apikey=${apiKey}&i=${id}`)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+      let movieObject = {
+        title: data.Title,
+        year: data.Year,
+        id: data.imdbID
+      }
+      listOfMovie.push(movieObject);
+      setLists(listOfMovie)
+    });
+
+    
     // try {
     //   await createList({
     //     variables: {  },
@@ -44,6 +58,7 @@ const Home = () => {
     //   console.error(err);
    // }
   }
+  // useEffect(() => setLists(listOfMovie), [])
 
   function apiCall(event)
   {
@@ -62,19 +77,25 @@ const Home = () => {
       color: '#F2A154'
     },
     blueColor: {
-      color: '#314E52'
+      color: '#314E52',
+
     },
     orangeColorBg: {
       backgroundColor: '#F2A154'
     },
     blueColorBg: {
       backgroundColor: '#314E52'
+    },
+    movieList: {
+      color: '#314E52' ,
+      backgroundColor: '#F2A154'
+
     }
   }
   const createMovieList = (event) =>
   {
     event.preventDefault();
-    setLists(listOfMovie);
+    console.log('yep')
   }
 
   return (
@@ -83,35 +104,35 @@ const Home = () => {
         
       <div className='container col-12 d-flex flex-column justify-content-center'>
         {loggedIn ?
-        <div className="d-flex flex-row">
-          <div className="col-6 d-flex flex-column">
-          <h1 style={styles.orangeColor}>Create A List!</h1> 
-          <div className="form-group d-flex flex-column mt-1 mb-1 col-sm-6">
-            <input 
-            value={listName}
-            onChange={handleInputChange}
-            type="text"
-            id="listName" 
-            name="listName"
-            placeholder="List Name" 
-            className="form-control justify-content-center align-items-center col-sm-12"/>
+        <div className="d-flex flex-row position-relative">
+          <h1 style={styles.orangeColor} className="position-relative">Create A List!</h1>
+          <div className="col-6 d-flex flex-column border rounded justify-content-center align-items-center m-2" > 
+            <div className="form-group d-flex flex-column mt-1 mb-1 col-sm-6">
+              <input 
+              value={listName}
+              onChange={handleInputChange}
+              type="text"
+              id="listName" 
+              name="listName"
+              placeholder="List Name" 
+              className="form-control justify-content-center align-items-center col-sm-12"/>
+            </div>
+            <div className="form-group d-flex flex-column mt-1 mb-1 col-sm-6">
+              <input 
+              value={listMsg}
+              onChange={handleInputChange}
+              type="text"
+              id="listMsg" 
+              name="listMsg"
+              placeholder="List Message" 
+              className="form-control justify-content-center align-items-center col-sm-12"/>
+            </div>
           </div>
-          <div className="form-group d-flex flex-column mt-1 mb-1 col-sm-6">
-            <input 
-            value={listMsg}
-            onChange={handleInputChange}
-            type="text"
-            id="listMsg" 
-            name="listMsg"
-            placeholder="List Message" 
-            className="form-control justify-content-center align-items-center col-sm-12"/>
-          </div>
-          </div>
-          <div className="d-flex flex-column list-group col-sm-6">
+          <div className="d-flex flex-column list-group col-sm-6 border rounded m-2">
             <button
               style={styles.orangeColorBg} 
               id="createList" 
-              type="submit" 
+              type="button" 
               className="btn d-flex justify-content-center align-items-center col-lg-6 m-auto mt-1"
               onClick={createMovieList}>
                 
@@ -120,8 +141,9 @@ const Home = () => {
             <div className="">
               {lists.map( list => 
                 <li
-                className="list-group-item"
-                key={list.imbdID+1}> {list.Title} </li>
+                className="list-group-item d-flex justify-content-center align-items-center fs-4"
+                style={styles.movieList}
+                key={list.id}> {list.title} </li>
               )}
             </div>
           </div>
