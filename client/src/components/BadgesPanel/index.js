2ecc66689c581f154ba3b263949934996ec2a1eb
Cornelia Herman <corneliabherman@gmail.com>
2022-02-08 20:50:37 -0500

    playing with list and accordion to the left

--- client/src/components/BadgesPanel/index.js
+++ client/src/components/BadgesPanel/index.js
@@ -3,48 +3,41 @@ import { useQuery } from '@apollo/client';
 
 import { GET_ME } from '../../utils/queries';
 
-const BadgesPanel = ({allMovieLists}) => {
-    const styles = {
-        listBadge: {
-        width: "20px",
-        height: "auto"
-        },
-        maxWidth: '540px'
-    }
+const BadgesPanel = ({ allMovieLists }) => {
+
     const [badge, setBadge] = useState([]);
     const { loading, data } = useQuery(GET_ME);
-    useEffect(() => {setBadgeImage()}, []);
-
-    const userCompletedLists = data?.me.completedLists || [];
+    useEffect(() => { setBadgeImage() }, []);
     
-    let completedMovieList = [];
+    const userCompletedLists = data?.me.completedLists || [];
     let hideBadgeImage = `/images/badges/Hidden-Badge.png`;
+    let completedMovieList = [];
+
     const setBadgeImage = () => {
         allMovieLists.forEach((listItem) => {
             let badgeImg = hideBadgeImage;
             let badgeId = listItem._id;
             let badgeName = listItem.name;
-            if (userCompletedLists.some((listData) => listData.listId === badgeId)) {
+            if (userCompletedLists.some((listData) => listData.listId === !badgeId)) {
                 badgeImg = listItem.badge;
             }
             completedMovieList.push({
                 src: badgeImg,
                 id: badgeId,
-                alt: badgeName})
+                alt: badgeName
+            })
         });
-        completedMovieList.forEach(toPrint => console.log(toPrint));
-        setBadge(completedMovieList)
+        setBadge(completedMovieList);
     };
-
     // populate all lists, if movie in users list, then show badge, if not show not badge
     return (
         <div className="card mb-3">
             <div className="flex-row justify-space-between my-4">
-                    {badge.map(award => 
-                    <div className="col-12 col-xl-6"><div styles={styles.listBadge} className="card mb-3"> <img src={process.env.PUBLIC_URL + award.src} key={award.id} alt={award.alt} />
+                {badge.map(award =>
+                    <div className="col-12 col-xl-6"><div className="listBadge mb-3"> {award.id} <img src={process.env.PUBLIC_URL + award.src} key={award.id} alt={award.alt} />
                     </div>
                     </div>
-                    )}
+                )}
             </div>
         </div>
     );
--- client/src/components/ListPanel/index.js
+++ client/src/components/ListPanel/index.js
@@ -1,24 +1,44 @@
-import React, { useState } from 'react';
-import { useQuery } from '@apollo/client';
+import React, { useEffect, useState } from 'react';
 
-import { GET_LISTS } from '../../utils/queries';
 
-const ListPanel = () => {
+const ListPanel = ({allMovieLists}) => {
+    let availableMovieLists = []
     const [list, setList] = useState([]);
-    const { loading, data } = useQuery(GET_LISTS);
+    useEffect(() => { setAllLists() }, [])
+console.log(allMovieLists);
+    const setAllLists = () => allMovieLists.forEach((movieList) => {
+        let listId = movieList._id;
+        let listName = movieList.name;
+        let listBadge = movieList.badge;
+        let listMovies = [movieList.movies];
+        availableMovieLists.push({
+            id: listId,
+            name: listName,
+            badge: listBadge,
+            movies: [listMovies]
+            });
+            setList(availableMovieLists);
+    });
 
     return (
         <div className="card mb-3">
             <div className="flex-row justify-space-between my-4">
-                {list.map((listItems) => (
-                        <div key={listItems} className="col-12 col-xl-6">
-                            <div className="card mb-3">
-                                <h4 className="card-header bg-dark text-light p-2 m-0">
-                                    {listItems} <br />
-                                </h4>
+                <div className="accordion accordion-flush" id="accordionFlushExample">
+                    {list.map((listItems) => (
+                        <div className="accordion-item">
+                            <h2 className="accordion-header" id="flush-headingOne">
+                                <button key={listItems.id} className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                                    <img src={listItems.badge} /> {listItems.name}
+                                </button>
+                            </h2>
+                            <div id="flush-collapseOne" className="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+                            {list.movies.map((movieItems) => (
+                                <div className="accordion-body"><ul><li key={movieItems.omdbId}>{movieItems.title} - {movieItems.year}</li></ul></div>
+                            ))}
                             </div>
                         </div>
                     ))}
+                </div>
             </div>
         </div>
     );
