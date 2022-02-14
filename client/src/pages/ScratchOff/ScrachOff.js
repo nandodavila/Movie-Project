9ac896e3a5572ef06713852f21240794d407881c
Justin Houk <jstndhouk@gmail.com>
2022-02-14 16:18:51 -0500

    followed video and adjusted as needed

--- client/src/pages/ScratchOff/ScrachOff.js
+++ client/src/pages/ScratchOff/ScrachOff.js
@@ -1,9 +1,18 @@
 import React, { useEffect } from 'react';
 import './style.css';
-import Jumbotron from '../components/Jumbotron';
 
 export default function ScratchOff() {
   
+  const styles = {
+    scratch: {
+      position:'relative',
+      width:'100%',
+      height: '100vh',
+      margin:'0',
+      padding:'0'
+    }
+  }
+
   return (
     <div>
       <header>
@@ -13,10 +22,19 @@ export default function ScratchOff() {
         <h2>
           You've completed listMessage
         </h2>
+        <div style={styles.scratch}>
+        <script src="https://code.jquery.com/jquery-2.2.4.js" integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI="crossorigin="anonymous"></script>
+          <script src="wScratchPad.min.js"></script>
+          <script type="text/javascript">
+          $('#scratch').wScratchPad({
+          size: 75,          // The size of the brush/scratch.
+          bg: '../../public/images/badges/Top-100-SciFi.png',  // Background (image path or hex color).
+          fg: '#6699ff',  // Foreground (image path or hex color).
+        });
+          </script>
+        </div>
       </header>
-        <h1>Success!</h1>
-        <h2>Thank you for your purchase!</h2>
-        <h2>You will now be redirected to the home page</h2>
+        <h1>Return to Dashboard</h1>
     </div>
   );
 }
