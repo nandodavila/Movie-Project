546e43f7700ac459e334e4f4ec9fbabccf5320c3
Justin Houk <jstndhouk@gmail.com>
2022-02-15 12:55:08 -0500

    created congrats page, in leui of scratchoff page

--- client/src/pages/ScratchOff/ScrachOff.js
+++ client/src/pages/ScratchOff/ScrachOff.js
@@ -2,12 +2,12 @@ import React, { useEffect } from 'react';
 import './style.css';
 import { GET_LIST } from '../../utils/queries';
 import { useQuery } from '@apollo/client';
-import $ from 'jquery';
+import { Link } from 'react-router-dom';
 export default function ScratchOff() {
   const listId='620b00000c02da49fc705620'
+  
   const {loading, error, data, refetch } = useQuery(GET_LIST, {variables: {listId}});
     const list = data?.list || {};
-    console.log(data)
   const styles = {
     scratch: {
       position:'relative',
@@ -15,34 +15,41 @@ export default function ScratchOff() {
       height: '100vh',
       margin:'0',
       padding:'0'
+    },
+    badgeImage: {
+      maxHeight: '300px',
+      maxWidth: '400px',
+      alignSelf: 'center'
+    },
+    link: {
+      textAlign:'center',
+      textDecoration: 'none',
+      color: '#F2A154',
+      fontSize: '20px'
     }
   };
 
   const imagePath="process.env.PUBLIC_URL' + '/images/badges/Top-100-SciFi.png";
-  const scratchObj= {
-    size: 75,          // The size of the brush/scratch.
-      bg: {imagePath},  // Background (image path or hex color).
-    fg: '#6699ff'  // Foreground (image path or hex color).
-  }
+  // const scratchObj= {
+  //   size: 75,          // The size of the brush/scratch.
+  //     bg: {imagePath},  // Background (image path or hex color).
+  //   fg: '#6699ff'  // Foreground (image path or hex color).
+  // }
 
   return (
-    <div>
-        <main className='column'>
-        <h1>
+    <div className='d-flex flex-column justify-content-center'>
+        <main className='d-flex flex-column justify-content-center'>
+        <h1 className='text-center'>
           {list.message}
         </h1>
-        <h2>
-          You've completed {list.name} 
+        <h2 className='text-center'>
+          Congradulations!  You've watched all the movies in the {list.name} list.
         </h2>
+        <img src={list.badge} style={styles.badgeImage} alt="BadgeIcon"/>
         </main>
-        <div id='scratch' className='column' style={styles.scratch}>
-        <script src="https://code.jquery.com/jquery-2.2.4.js" integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI="crossOrigin="anonymous"></script>
-          <script src="wScratchPad.min.js"></script>
-          <script type="text/javascript">
-          $('#scratch').wScratchPad({scratchObj}
-          </script>
-        </div>
-        <h1>Return to Dashboard</h1>
+        <br></br>
+        <Link style={styles.link}to="/dashboard">Return to Dashboard</Link>
+
     </div>
   );
 }
